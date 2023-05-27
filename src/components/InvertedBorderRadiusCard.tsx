e1741c3c904da6a93ec896cffb44036df9f2e3b4
Arturo Rivera <arturo.riverar97@gmail.com>
2023-05-27 00:30:27 -0600

    Set InvertedBorderRadiusCard component

---
+++ src/components/InvertedBorderRadiusCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/InvertedBorderRadiusCard.css'
+
+const InvertedBorderRadiusCard = () => {
+  return (
+    <div className='main_container'>
+        <div className='card'>
+            <div className='image'></div>
+            <div className='content'>
+                <div className="extension"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default InvertedBorderRadiusCard
