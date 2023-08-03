7c140f69c260b8b7282f9d09b9d6a41c64cdec7f
Arturo <arturo.riverar97@gmail.com>
2023-08-03 08:52:50 -0600

    Add FormBorderAnimation exclusive class in parent div

--- src/components/FormBorderAnimation.tsx
+++ src/components/FormBorderAnimation.tsx
@@ -2,17 +2,19 @@ import '../styles/FormBorderAnimation.css'
 
 const FormBorderAnimation = () => {
   return (
-    <div className='box'>
-        <form>
-            <h2>Sign in</h2>
-            <input type="text" placeholder='Username'/>
-            <input type="password" placeholder='Password'/>
-            <input type="submit" value='Login'/>
-            <div className="group">
-                <a href="">Forget Password</a>
-                <a href="">Sign up</a>
-            </div>
-        </form>
+    <div className="main_container form__animation">
+        <div className='box'>
+            <form>
+                <h2>Sign in</h2>
+                <input type="text" placeholder='Username'/>
+                <input type="password" placeholder='Password'/>
+                <input type="submit" value='Login'/>
+                <div className="group">
+                    <a href="">Forget Password</a>
+                    <a href="">Sign up</a>
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
