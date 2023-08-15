e6ead44cbec0cb7ff14c0217236fffb46aa9c137
Arturo <arturo.riverar97@gmail.com>
2023-08-15 08:04:10 -0600

    Create RainText component and comment Netflix component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Home from './components/Home'
 import InvertedBorderRadiusCard from './components/InvertedBorderRadiusCard'
 import Navbar from './components/Navbar'
 import NetflixLines from './components/NetflixLines'
-
+import RainText from './components/RainText'
+//      <NetflixLines />
 function App() {
   return (
     <div className='app'>
       <InvertedBorderRadiusCard />
       <FormBorderAnimation />
-      <NetflixLines />
+      <RainText />
     </div>
   )
 }
---
+++ src/components/RainText.tsx
@@ -0,0 +1,11 @@
+import "../styles/RainText.css"
+
+const RainText = () => {
+  return (
+    <div className='rain__text-container main_container'>
+        <div className="cloud"></div>
+    </div>
+  )
+}
+
+export default RainText
