15927df24c15fe45623282913921cc163b29e0a5
Arturo <arturo.riverar97@gmail.com>
2023-09-21 08:28:26 -0600

    Import TextAnimation component

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import InvertedBorderRadiusCard from './components/InvertedBorderRadiusCard'
 import Navbar from './components/Navbar'
 import NetflixLines from './components/NetflixLines'
 import RainText from './components/RainText'
+import TextAnimation from './components/TextAnimation'
 //      <NetflixLines />
 function App() {
   return (
     <div className='app'>
+      <TextAnimation />
       <HotCup />
       <GalleryHover />
       <InvertedBorderRadiusCard />
